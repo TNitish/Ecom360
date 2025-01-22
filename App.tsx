d7c2d9b099bb5cf7b6b69358c5a8122305a432f2
nitish <nitishthangaraj@gmail.com>
2025-01-22 15:39:13 +0530

    anand name chnged

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity, Alert } from 'react-native'
 import React from 'react'
 
 export default function App() {
   return (
     <View>
+      <TouchableOpacity onPress={()=>Alert.alert("duck guna ")}/>
       <Text>welcome nithishuuu</Text>
       <Text>WELCOME ANANDHUUUUU GUNAAAA TAMILUUU</Text>
+      </TouchableOpacity>
     </View>
   )
 }
